d59cad2690d358fd1e349ceff7a15a257d388af0
BarZu <cjlice@qq.com>
2017-02-03 15:35:46 +0800

    抽离jtaro-bundle.js

--- demos/jroll_build/build.js
+++ demos/jroll_build/build.js
@@ -1,21 +1,16 @@
-var fs = require('fs')
 var rollup = require('rollup')
 var path = require('path')
 var jtaroModule = require('rollup-plugin-jtaro-module')
+var jtaroBundle = require('jtaro-bundle')
 
 // 遍历demos/jroll_demo/pages里的js文件路径创建bundle.js
-var paths = fs.readdirSync(path.resolve('demos/jroll_demo/pages'))
-var content = ''
-
-paths.forEach((item, index) => {
-  if (/\.js$/.test(item)) {
-    content += 'import v' + index + ' from \'./pages/' + item + '\'\nVue.component(\'pages__' + item.replace('.js', '') + '\', v' + index + ')\n'
-  }
+jtaroBundle.bundle({
+  src: 'demos/jroll_demo/pages',
+  dest: 'demos/jroll_demo/pages.js'
 })
-fs.writeFileSync(path.resolve('demos/jroll_demo/bundle.js'), content)
 
 rollup.rollup({
-  entry: path.resolve('demos/jroll_demo/bundle.js'),
+  entry: path.resolve('demos/jroll_demo/pages.js'),
   plugins: [jtaroModule({ root: 'demos' })]
 }).then(function (bundle) {
   bundle.write({
